46dfc78c34f96c8d6b79b485d8227f8f767a206a
Patrick Meier <patrick.meier@komaxgroup.com>
2025-04-04 09:08:10 +0200

    Aufgabe 6.3: test definitions adapted for median

--- src/median.test.ts
+++ src/median.test.ts
@@ -1,16 +1,17 @@
 import { mean } from "./average";
+import { median } from "./median";
 
 describe("median", () => {
   test("test median for odd number of elements", () => {
-    const numbers: number[] = [1, 2, 3];
-    const expected: number = 0.0;
+    const numbers: number[] = [1, 2, 3, 4, 5, 6, 7];
+    const expected: number = 4;
     const actual: number = median(numbers);
     expect(actual).toBe(expected);
   });
 
   test("test median for even number of elements", () => {
-    const numbers: number[] = [1, 2, 3, 4];
-    const expected: number = 0.0;
+    const numbers: number[] = [1, 2, 3, 4, 5, 6];
+    const expected: number = 3.5;
     const actual: number = median(numbers);
     expect(actual).toBe(expected);
   });
@@ -20,7 +21,3 @@ describe("median", () => {
     expect(() => median(numbers)).toThrow();
   });
 });
-
-function median(numbers: number[]): number {
-  return 0.0;
-}
